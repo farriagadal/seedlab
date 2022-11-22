baedcf5e736368d53ee867b7bb2974d5e1b16453
Fernando <farriagadal94@gmail.com>
2022-11-22 00:46:04 -0300

    add fb pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,26 @@ import type { AppProps } from 'next/app'
 import Transition from 'src/components/Transition'
 import Footer from 'src/components/Footer'
 import Header from 'src/components/Header'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init('3082924931783292') // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+  }, [router.events])
+
   return (
     <>
       <Header />
@@ -16,3 +34,20 @@ export default function App({ Component, pageProps }: AppProps) {
 
   )
 }
+// <!-- Meta Pixel Code -->
+// <script>
+// !function(f,b,e,v,n,t,s)
+// {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+// n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+// if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+// n.queue=[];t=b.createElement(e);t.async=!0;
+// t.src=v;s=b.getElementsByTagName(e)[0];
+// s.parentNode.insertBefore(t,s)}(window, document,'script',
+// 'https://connect.facebook.net/en_US/fbevents.js');
+// fbq('init', '3082924931783292');
+// fbq('track', 'PageView');
+// </script>
+// <noscript><img height="1" width="1" style="display:none"
+// src="https://www.facebook.com/tr?id=3082924931783292&ev=PageView&noscript=1"
+// /></noscript>
+// <!-- End Meta Pixel Code -->
